3a27c47c355fd736248591500f8447ccd69eebc7
R4MA26 <ramaismaya25@gmail.com>
2022-04-23 15:26:27 +0800

    flutter tes

--- apps/flutter-tes/test/widget_test.dart
+++ apps/flutter-tes/test/widget_test.dart
@@ -1,31 +1,48 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter_tes/app/widgets/widgets.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:get/get.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// void main() {
+//   // Define a test. The TestWidgets function also provides a WidgetTester
+//   // to work with. The WidgetTester allows building and interacting
+//   // with widgets in the test environment.
+//   testWidgets("Flutter Widget Test", (WidgetTester tester) async {
+//     await tester.pumpWidget(const TextAccount(
+//       textButton: 'test',
+//       account: 'test',
+//     ));
+//     var text = find.text('test');
+//     var text2 = find.text('test');
 
-import 'package:flutter_tes/main.dart';
-import 'package:get/get.dart';
+//     expect(text, findsOneWidget);
+//     expect(text2, findsOneWidget);
+//   });
+// }
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const GetMaterialApp());
+// // class MyWidget extends StatelessWidget {
+// //   const MyWidget({
+// //     Key? key,
+// //     required this.title,
+// //     required this.message,
+// //   }) : super(key: key);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+// //   final String title;
+// //   final String message;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'Flutter Demo',
+// //       home: Scaffold(
+// //         appBar: AppBar(
+// //           title: Text(title),
+// //         ),
+// //         body: Center(
+// //           child: Text(message),
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
